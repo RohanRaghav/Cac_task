0e1ad4355b4c07f065e30fa43eb62e53481436e0
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2024-07-29 23:26:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import Background from './Components/Background';
+import Cac from './Components/Cac';
+import Sidebar from './Components/SideBar';
 function App() {
+  const [visibleIds, setVisibleIds] = useState([]);
+
+  const handleInView = (id) => {
+    setVisibleIds(prevIds => {
+      if (!prevIds.includes(id)) {
+        return [...prevIds, id];
+      }
+      return prevIds;
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<img src='Cac_logo.png' alt='abc' className='image' />
+<Sidebar visibleIds={visibleIds} />
+<Cac id="About" onInView={handleInView} />
+      <Background />
     </div>
+
   );
 }
 
